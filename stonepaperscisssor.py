f69e7dd9b2f1719480c35fba398d560c54f791cd
Simon Engelhardt <simon.j.engel@gmail.com>
2018-06-20 14:44:13 +0200

    Transleted to english

--- stonepaperscisssor.py
+++ stonepaperscisssor.py
@@ -2,25 +2,26 @@ import random
 def paperscissorstone(user_input):
     computer = random.randint(0,2)
     if computer == 0:
-        bot = "Papier"
+        bot = "paper"
     elif computer == 1:
-        bot = "Schere"
+        bot = "scissor"
     elif computer == 2:
-        bot = "Stein"
+        bot = "stone"
     if user_input == bot:
-        print("Unentschieden")
-    elif user_input == "Stein" and bot == "Papier":
-        print("Du hast verloren, der Computer hat Papier gewählt")
-    elif user_input == "Stein" and bot == "Schere":
-        print("You won, bot chose Schere")
-    elif user_input == "Papier" and bot == "Stein":
-        print("You won, bot chose Stein")
-    elif user_input == "Papier" and bot == "Stein":
-        print("Du hast verloren, der Computer hat Stein gewählt")
-    elif user_input == "Schere" and bot == "Stein":
-        print("Du hast verloren, der Computer hat Stein gewählt")
-    elif user_input == "Schere" and bot == "Papier":
-        print("You won, bot chosee Papier")
+        print("tie")
+    elif user_input == "stone" and bot == "paper":
+        print("You lost, the computer chose 'paper' ")
+    elif user_input == "stone" and bot == "scissor":
+        print("You won, the computer chose 'scissor'")
+    elif user_input == "paper" and bot == "stone":
+        print("You won, the computer chose stone")
+    elif user_input == "paper" and bot == "stone":
+        print("You lost, the computer chose stone gewählt")
+    elif user_input == "scissor" and bot == "stone":
+        print("You lost, the computer chose stone gewählt")
+    elif user_input == "scissor" and bot == "paper":
+        print("You won, the computer chose 'paper'")
     else:
         print("You typed the wrong thing")
-paperscissorstone(input("Please choose Papier, Stein or Schere "))
+while True:
+    paperscissorstone(input("Please choose paper, stone or scissor "))
